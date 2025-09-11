0c9f204e43abfb9a404f06709cf39a0fb5562ee4
Ricardo Rocha <ricardo@plenix.us>
2025-09-11 18:26:29 +0000

    Formulating base type hierarchy

--- filer.js
+++ filer.js
@@ -1,28 +1,44 @@
 // explore filer
 
+const Type = {};
+const ValueType = {};
+const ObjectType = {};
+const EntityType = {};
+
+const assert = console.assert
+
+const names = new Set();
+function type({name, description}) {
+    if (names.has(name)) {
+        throw "Name taken: " + name;
+    } else {
+        names.add(name);
+    }
+    return {name, description};
+}
+
+function simpleSerializer(parse, format = o => o.toString()) {
+    return { parse, format };
+};
+
 function valueType(options = {name, description, serializer}) {
-    const self = type(name, description); // add to global namespace
     return {
-        ...options, ...self,
+        ...type({
+            name: options.name,
+            description: options.description
+        }),
+        ...options,
         kind: "ValueType",
-        serializer: serializer,
-        format: serializer.format,
-        parse: serializer.parse,
+        serializer: options.serializer,
+        format: options.serializer.format,
+        parse: options.serializer.parse,
     };
 }
 const int = valueType({
     name: "int",
     description: "Integer value type",
-    serializer: simpleSerializer({
-        parse: s => parseInt(s),
-        format: i => i.toString()
-    }),
+    serializer: simpleSerializer(s => parseInt(s)),
 });
 
-assert(
-    int.parse("123.456") === 123,
-    "Doesn't parse string '123.456' int into int 123");
-assert(
-    int.format(654 === "654"),
-    "Doesn't format int 654 int into string '654'");
-metaschema.int = int;
+assert(int.parse("123.456") === 123, "Bad integer parsing");
+assert( int.format(654 === "654"), "Bad integer formatting");
